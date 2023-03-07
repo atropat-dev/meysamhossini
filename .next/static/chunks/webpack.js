37a2ccd65e11f22fcdc3f6788eef7be4bb749f6f
atropat-dev <hosseinmohammadidev@gmail.com>
2023-03-07 17:29:48 +0330

    2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c3ad23392ac439f"; }
+/******/ 		__webpack_require__.h = function() { return "f76f53ae14295bb7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
